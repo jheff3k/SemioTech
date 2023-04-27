3e6df585ab79063c867ffffe647a690c57322c55
jheff3k <129636583+jheff3k@users.noreply.github.com>
2023-04-26 22:58:04 -0300

    Add files via upload

---
+++ src/model/data.js
@@ -0,0 +1,15 @@
+export const sliderData = [
+    {
+      title: 'First Game',
+      image: require ('../assets/homescreen/bot-1.png'),
+    },
+    {
+      title: 'Second Game',
+      image: require('../assets/homescreen/bot-2.png'),
+    },
+    {
+      title: 'Third Game',
+      image: require('../assets/homescreen/bot-3.png'),
+    },
+  ];
+  
